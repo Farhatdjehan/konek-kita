1d990b8284776044318f8b189e5cd200930d6cfe
farhatdk <farhatdk98@gmail.com>
2021-11-25 15:10:26 +0700

    push

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -76,7 +76,7 @@ const Login = ({ isSignUp }) => {
 													setUsernameInput(false);
 													setIsNewUser(!isNewUser);
 												}}>
-												Sign In
+												Sign Up
 											</Button>
 										</div>
 									</div>
